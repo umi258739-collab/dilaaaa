ba1aa6f6085101c3534d8ff1ba35e6a50916b09d
umi258739-collab <umi258739@student.smkn1kandeman.sch.id>
2025-11-24 09:32:28 +0700

    Add files via upload

---
+++ Praktik6.1_UMI FADILAH NUR AISYAH_X PPLG 1/Praktik6.1_UMI FADILAH NUR AISYAH_X PPLG 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._1_UMI_FADILAH_NUR_AISYAH_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat array dengan 5 elemen tipe data string
+            string[] siswa = new string[5];
+
+            // Mengisi elemen array
+            siswa[0] = "Andi";
+            siswa[1] = "Budi";
+            siswa[2] = "Citra";
+            siswa[3] = "Dewi";
+            siswa[4] = "Eka";
+
+            // Menampilkan isi array tanpa perulangan
+            Console.WriteLine("Daftar Nama Siswa:");
+            Console.WriteLine(" Siswa ke-1: " + siswa[0]);
+            Console.WriteLine(" Siswa ke-2: " + siswa[1]);
+            Console.WriteLine(" Siswa ke-3: " + siswa[2]);
+            Console.WriteLine(" Siswa ke-4: " + siswa[3]);
+            Console.WriteLine(" Siswa ke-5: " + siswa[4]);
+        }
+    }
+}
