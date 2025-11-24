934869bb8d9efebdadf6909063fb51896e41cb97
umi258739-collab <umi258739@student.smkn1kandeman.sch.id>
2025-11-24 09:32:59 +0700

    Add files via upload

---
+++ Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1/Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._2_UMI_FADILAH_NUR_AISYAH_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat array integer dengan 5 elemen
+            int[] angka = new int[5];
+
+            // Input elemen array dari kyboard
+            Console.Write("Masukkan nilai ke-1: ");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-2: ");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-3: ");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-4: ");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-5: ");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+
+            // Menampilkan isi array tanpa perulangan
+            Console.WriteLine("\nData yang anda masukkan:");
+            Console.WriteLine("Nilai ke-1: " + angka[0]);
+            Console.WriteLine("Nilai ke-2: " + angka[1]);
+            Console.WriteLine("Nilai ke-3: " + angka[2]);
+            Console.WriteLine("Nilai ke-4: " + angka[3]);
+            Console.WriteLine("Nilai ke-5: " + angka[4]);
+        }
+    }
+}
---
+++ Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1/Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktik6.2_UMI FADILAH NUR AISYAH_X PPLG 1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e96db85f-8c8d-451f-8d26-121f563c68c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
