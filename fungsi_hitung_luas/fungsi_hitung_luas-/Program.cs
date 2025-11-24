3ff3aa3ebde4062427dd3b02233d8cb6d149327b
umi258739-collab <umi258739@student.smkn1kandeman.sch.id>
2025-11-24 09:33:41 +0700

    Add files via upload

---
+++ fungsi_hitung_luas/fungsi_hitung_luas-/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fungsi_hitung_luas_
+{
+    internal class Program
+    {
+        static double HitungLuas(double panjang, double lebar)
+        {
+            double luas = panjang * lebar;
+            return luas;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan Panjang");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Lebar");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            double luas = HitungLuas(p, l);
+            Console.WriteLine("Luas Persegi Panjang: " + luas);
+        }
+    }
+}
