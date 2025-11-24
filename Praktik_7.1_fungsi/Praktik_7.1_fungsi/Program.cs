c756b8f7bea3e5485dd98d3d5ab8f3c97be7fdfd
umi258739-collab <umi258739@student.smkn1kandeman.sch.id>
2025-11-24 09:30:40 +0700

    Add files via upload

---
+++ Praktik_7.1_fungsi/Praktik_7.1_fungsi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_7._1_fungsi
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+          Console.WriteLine("Halo, selamat datang di pemrograman C#!");
+          Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil fungsi
+        }
+    }
+}
